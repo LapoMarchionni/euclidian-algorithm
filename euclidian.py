5f8c03adf0d76ce43a6c1839ec9696d758acf622
Lapo Marchionni <marchionni.lapo@gmail.com>
2018-04-12 11:00:39 +0200

    GDC

--- euclidian.py
+++ euclidian.py
@@ -1,11 +1,11 @@
 def gdc(a, b):
-    """Recursively calculate the GDC between two positive 
+    """Recursively calculate the GDC between two positive
     integer numbers.
-    a is always setted as the biggest of the two while b is the smallest.
+    If a < b, the first recursion will exchange both numbers.
     """
-    assert a > 0 and b > 0, ("Both number must be positive integers")
-    a, b = max(a, b), min(a, b)
-    if a % b == 0:
+    assert a >= 0 and b >= 0, (
+        "Both number must be positive integers")
+    if b == 0:
         return b
     return gdc(b, a % b)
 
